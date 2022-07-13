81b0ea645b48d5a1539dc1ca5b2192291289ef52
cwx <2352265318@qq.com>
2022-07-13 16:30:26 +0800

    测试类Test

---
+++ src/com/cwx/Test.java
@@ -0,0 +1,13 @@
+package com.cwx;
+
+/**
+ * @author : cwx
+ * @date : 2022-07-13 16:25:37
+ * @version: 1.0
+ **/
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("测试类");
+    }
+}
